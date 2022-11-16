ad3a943301a476b461015e7ffd91646f85d18c93
Petr Katerinak <petrkaterinak@inove.cz>
2022-11-16 08:23:12 +0100

    Added blacklisted check

--- app/Http/Controllers/API/ScoringEngineController.php
+++ app/Http/Controllers/API/ScoringEngineController.php
@@ -1,11 +1,14 @@
 <?php
+
 declare(strict_types=1);
+
 namespace App\Http\Controllers\API;
 
 use App\Models\LoginAttemp;
 use Illuminate\Http\Request;
 use App\Services\LoginService;
 use Illuminate\Http\JsonResponse;
+use App\Services\BlacklistService;
 use App\Services\ScoreEntityService;
 use App\Http\Controllers\Controller;
 use App\Services\ScorePasswordService;
@@ -15,18 +18,31 @@ class ScoringEngineController extends Controller
     public function __construct(
         protected ScorePasswordService $scorePasswordService,
         protected ScoreEntityService $scoreEntityService
-    ) {}
+    ) {
+    }
 
     public function scoreLogin(Request $request): JsonResponse
     {
         $password = $request->input('password');
+        $email = $request->input('email');
         $entity = $request->input('hash');
         $userAgent = $request->input('user-agent');
         $clientIp = $request->input('ip');
         $clientIp = $clientIp === '127.0.0.1' ? '46.135.97.82' : $clientIp;
 
         if (!$entity || !$password) {
-            return response()->json(['error' => 'Unsufficient data provided']);
+            return $this->error('Unsufficient data provided');
+        }
+
+        $blacklistService = new BlacklistService();
+        [$blacklisted, $blacklistType, $value, $blaclistId] = $blacklistService->isBlacklisted($email, $clientIp);
+        if ($blacklisted) {
+            return $this->error([
+                'error' => 'Entity is blacklisted',
+                'value' => $value,
+                'blacklist_type' => $blacklistType,
+                'blacklist_id' => $blaclistId,
+            ]);
         }
 
         try {
@@ -42,7 +58,7 @@ class ScoringEngineController extends Controller
             $scoreEntity = $this->scoreEntityService->scoreEntity($entity, $loginData);
             $loginScore += $scoreEntity['score'];
 
-            $loginAttemp = LoginAttemp::create($loginData);
+            LoginAttemp::create($loginData);
 
             return response()->json(['score' => $loginScore, 'password' => $scorePassword, 'entity' => $scoreEntity]);
         } catch (\Throwable $throwable) {
--- app/Services/BlacklistService.php
+++ app/Services/BlacklistService.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
+use App\Models\Blacklist;
+use App\Enums\BlacklistTypeEnum;
 use Illuminate\Support\Collection;
 use App\Http\Resources\BlacklistResource;
 
@@ -18,5 +22,52 @@ class BlacklistService
             'next_page' => $pageLength * $page < $total ? $page + 1 : null,
             'prev_page' => $page > 1 ? $page - 1 : null,
         ]);
+    }
+
+    public function isBlacklisted(string $email, string $ip): array
+    {
+        $explodedEmail = explode('@', $email);
+        $domain = array_pop($explodedEmail);
+
+        if ($blacklistId = $this->checkInBlacklist($ip, BlacklistTypeEnum::IP)) {
+            return [true, BlacklistTypeEnum::IP, $ip, $blacklistId];
+        }
+
+        if ($blacklistId = $this->checkInBlacklist($domain, BlacklistTypeEnum::DOMAIN)) {
+            return [true, BlacklistTypeEnum::DOMAIN, $domain, $blacklistId];
+        }
+
+        if ($blacklistId = $this->checkInBlacklist($email, BlacklistTypeEnum::EMAIL)) {
+            return [true, BlacklistTypeEnum::EMAIL, $email, $blacklistId];
+        }
+
+        return [false, null, null, null];
+    }
+
+    private function checkInBlacklist(string $value, BlacklistTypeEnum $blacklistType): ?int
+    {
+        $value = json_encode($value);
+        if ($blacklistType !== BlacklistTypeEnum::IP && $blacklist = Blacklist::whereActive(true)->whereType($blacklistType)->whereValue($value)->first()) {
+            return $blacklist->getId();
+        }
+
+        if ($blacklistType === BlacklistTypeEnum::IP) {
+            if ($blacklist = Blacklist::whereActive(true)->whereType($blacklistType)->where('value', 'LIKE', '%'.$value.'%')->first()) {
+                return $blacklist->getId();
+            }
+
+            $longIP = ip2long($value);
+            $blacklistsWithRanges = Blacklist::whereActive(true)->whereType($blacklistType)->where('value', 'LIKE', '[%')->get();
+            /** @var Blacklist $blacklist */
+            foreach ($blacklistsWithRanges as $blacklist) {
+                $range = array_map(static fn($ip) => ip2long($ip), $blacklist->getValue());
+                $rangeMin = min($range);
+                $rangeMax = max($range);
+                if ($longIP >= $rangeMin && $longIP <= $rangeMax) {
+                    return $blacklist->getId();
+                }
+            }
+        }
+        return null;
     }
 }
